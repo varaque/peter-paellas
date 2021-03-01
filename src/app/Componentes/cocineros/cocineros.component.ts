6e6624025e9253002ad3c6d5a5521db60b40d357
Willian Galvez <wgalvez@circulorojo.es>
2021-03-01 17:50:20 +0100

    maquetando página de cocineros

--- src/app/Componentes/cocineros/cocineros.component.ts
+++ src/app/Componentes/cocineros/cocineros.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-//import { COCINEROS } from '../cocinerosprueba';
-import { Cocinero } from './cocinero';
-
 import { UserService } from 'src/app/services/user.service';
 import { HttpClient } from '@angular/common/http';
-import {User} from '../../Interfaces/user';
+import { User } from '../../Interfaces/user';
 
 @Component({
   selector: 'app-cocineros',
@@ -12,22 +9,16 @@ import {User} from '../../Interfaces/user';
   styleUrls: ['./cocineros.component.css']
 })
 export class CocinerosComponent implements OnInit {
-
-//  cocineros = COCINEROS;
-
-  API_ENDPOINT ='http://localhost:8000/api/users'; // SI TE DA PROBLEMA AL CARGARLAS O ALGO PUEDE SER POR EL CORS ESE QUE PONE EN EL F12, QUE ES QUENO ESTAS EN WHITE LIST PARA EL SERVER, GASTA LA EXTENSION DE CHROME ALLOW CORS Y AU
+  /*  SI TE DA PROBLEMA AL CARGARLAS O ALGO PUEDE SER POR EL CORS 
+  ESE QUE PONE EN EL F12, QUE ES QUENO ESTAS EN WHITE LIST PARA EL SERVER, 
+  GASTA LA EXTENSION DE CHROME ALLOW CORS Y AU */
+  API_ENDPOINT = 'http://localhost:8000/api/users';
   users: User[];
-
-  constructor(private userService: UserService, private httpClient: HttpClient) { 
-
-    httpClient.get( this.API_ENDPOINT).subscribe((data: User[]) => {
+  constructor(private userService: UserService, private httpClient: HttpClient) {
+    httpClient.get(this.API_ENDPOINT).subscribe((data: User[]) => {
       this.users = data;
-      console.log(data)
     })
-
   }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
