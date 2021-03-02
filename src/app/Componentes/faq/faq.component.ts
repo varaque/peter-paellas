6b5c8d0c885e3d3ac70aa6b27668cf6cab70d3db
Willian Galvez <wgalvez@circulorojo.es>
2021-03-02 12:37:43 +0100

    preguntas frecuentes

--- src/app/Componentes/faq/faq.component.ts
+++ src/app/Componentes/faq/faq.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
+  modal_activated: boolean
 
-  constructor() { }
+  constructor() {
+    this.modal_activated = false
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleModal() {
+    this.modal_activated ? this.modal_activated = false : this.modal_activated = true;
   }
 
 }
